56f0e406f0d0bfccbdb2a2e93b04595773356ec7
RAFLI <raflijjunior@gmail.com>
2024-10-06 07:33:37 +0700

    hide send wa

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -364,14 +364,14 @@ Terima kasih`;
              </h1>  
         </div>
         </div>  
-        <div class="absolute left-0 right-0 bottom-4 px-[1em] flex justify-center items-center">
+        <!-- <div class="absolute left-0 right-0 bottom-4 px-[1em] flex justify-center items-center">
             <button 
             on:click={sendWA}
             class="rounded-xl py-2 px-4 bg-green-500 flex items-center text-white font-bold">
                 <span class="mr-1">
                     <Icon icon="ic:baseline-whatsapp" width="24" height="24" />
                 </span> Kirim ke Whatsapp</button>
-        </div>
+        </div> -->
     {/if}
 </Modal>
 
